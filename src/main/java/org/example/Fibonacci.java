77201f1560a334cc0c828d20693042fa67d8a2c2
Jan Reinhard <jan.reinhard@rwth-aachen.de>
2024-08-20 11:48:17 +0200

    add more test cases and adjust implementation

--- src/main/java/org/example/Fibonacci.java
+++ src/main/java/org/example/Fibonacci.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class Fibonacci {
     public static int fib(int n) {
-        return n;
+        if(n<=1){
+            return n;
+        }
+        return fib(n-1)+fib(n-2);
     }
 }
--- src/test/java/org/example/FibonacciTest.java
+++ src/test/java/org/example/FibonacciTest.java
@@ -7,12 +7,37 @@ import static org.junit.jupiter.api.Assertions.*;
 class FibonacciTest {
 
     @Test
-    void fib0ShouldBe0(){
-        assertEquals(0,Fibonacci.fib(0));
+    void fib0ShouldBe0() {
+        assertEquals(0, Fibonacci.fib(0));
     }
 
     @Test
-    void fib1ShouldBe1(){
-        assertEquals(0,Fibonacci.fib(0));
+    void fib1ShouldBe1() {
+        assertEquals(1, Fibonacci.fib(1));
+    }
+
+    @Test
+    void fib2ShouldBe1() {
+        assertEquals(1, Fibonacci.fib(2));
+    }
+
+    @Test
+    void fib3ShouldBe2() {
+        assertEquals(2, Fibonacci.fib(3));
+    }
+
+    @Test
+    void fib4ShouldBe3() {
+        assertEquals(3, Fibonacci.fib(4));
+    }
+
+    @Test
+    void fib5ShouldBe5() {
+        assertEquals(5, Fibonacci.fib(5));
+    }
+
+    @Test
+    void fib6ShouldBe8() {
+        assertEquals(8, Fibonacci.fib(6));
     }
 }
